24fa094ca091360f0a5918878aa71cb3c211e84f
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-02-05 17:14:42 -0600

    Add Country component and update UI

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Medal from './Medal';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 
 class Country extends Component {
   getMedalsTotal(country, medals) {
@@ -10,24 +12,36 @@ class Country extends Component {
   render() { 
     const { country, medals, onIncrement, onDecrement, onDelete } = this.props;
     return (
-      <div className="country">
-        <div className="name">
-          { country.name }
-          <span className="badge">
-            { this.getMedalsTotal(country, medals) }
-          </span>
-        </div>
-        { medals.map(medal =>
-          <Medal 
-            key={ medal.id } 
-            country={ country } 
-            medal={ medal } 
-            onIncrement={ onIncrement } 
-            onDecrement={ onDecrement } />
-        ) }
-        <button onClick={() => onDelete(country.id)}>delete</button>
-        <hr />
-      </div>
+      <Card>
+        <Card.Body>
+          <Card.Title className="d-flex justify-content-between">
+            <span>
+            { country.name }
+            <Badge bg="secondary" text="light" pill className="ml-2">
+              { this.getMedalsTotal(country, medals) }
+            </Badge>
+            </span>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+      // <div className="country">
+      //   <div className="name">
+      //     { country.name }
+      //     <span className="badge">
+      //       { this.getMedalsTotal(country, medals) }
+      //     </span>
+      //   </div>
+      //   { medals.map(medal =>
+      //     <Medal 
+      //       key={ medal.id } 
+      //       country={ country } 
+      //       medal={ medal } 
+      //       onIncrement={ onIncrement } 
+      //       onDecrement={ onDecrement } />
+      //   ) }
+      //   <button onClick={() => onDelete(country.id)}>delete</button>
+      //   <hr />
+      // </div>
     );
   }
 }
