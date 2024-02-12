0faa8c1f735e3581a989bca1492b8b6c45b3003c
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-02-11 23:21:38 -0600

    Refactor NewCountry component to use functional components and hooks

--- src/components/NewCountry.jsx
+++ src/components/NewCountry.jsx
@@ -1,63 +1,65 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import { PlusCircleFill } from 'react-bootstrap-icons';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+// import Modal from 'react-bootstrap/Modal';
+import Form from "react-bootstrap/Form";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
+import { PlusCircleFill } from "react-bootstrap-icons";
 
+const NewCountry = (props) => {
+	const [show, setShow] = useState(false);
+	const [newCountryName, setNewCountryName] = useState("");
+	const handleToastClose = () => {
+		setShow(false);
+		setNewCountryName("");
+	};
+	const handleToastKeyPress = (e) =>
+		(e.keyCode ? e.keyCode : e.which) === 13 && handleAdd();
+	const handleAdd = () => {
+		newCountryName.length > 0 && props.onAdd(newCountryName);
+		handleToastClose();
+	};
 
-class NewCountry extends Component {
-  state = {
-    showModal: false,
-    newCountryName: "",
-  }
-  handleModalClose = () => this.setState({ showModal: false });
-  handleModalKeyPress = (e) => (e.keyCode ? e.keyCode : e.which) === 13 && this.handleAdd();
-  handleAdd = () => {
-    this.state.newCountryName.length > 0 && this.props.onAdd(this.state.newCountryName);
-    this.handleModalClose();
-  }
-  handleClick = () => {
-    const name = prompt('Enter country name');
-    if (name && name.trim().length > 0) {
-      this.props.onAdd(name);
-    }
-  }
-  render() { 
-    return (
-      <React.Fragment>
-        <Button variant="outline-success" onClick={ () => this.setState({ showModal: true, newCountryName: "" })}>
-          <PlusCircleFill />
-        </Button>
-        <Modal onKeyPress={ this.handleModalKeyPress } show={ this.state.showModal } onHide={ this.handleModalClose }>
-          <Modal.Header closeButton>
-            <Modal.Title>New Country</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form.Group className="mb-3" controlId="modalForm1">
-              <Form.Label>Country Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="newCountryName"
-                onChange={ (e) => this.setState({ [e.target.name]: e.target.value}) }
-                value={ this.state.newCountryName }
-                placeholder="enter name"
-                autoFocus
-                autoComplete='off'
-              />
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={ this.handleModalClose }>
-              Close
-            </Button>
-            <Button variant="primary" onClick={ this.handleAdd }>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </React.Fragment>
-    );
-  }
-}
+	useEffect(() => {
+		if (show) {}
+	}, [show, newCountryName]);
+
+	return (
+		<React.Fragment>
+			<Button
+				variant="outline-success"
+				onClick={() => setShow(true)}>
+				<PlusCircleFill />
+			</Button>
+			<ToastContainer
+				className="p-2"
+				position="top-end"
+				style={{ zIndex: 1 }}>
+				<Toast
+					onClose={() => handleToastClose()}
+					show={show}
+					delay={3000}
+					onKeyDown={handleToastKeyPress}>
+					<Toast.Header>
+						<strong className="me-auto">Country Name Required</strong>
+					</Toast.Header>
+					<Toast.Body>
+						<Form.Group controlId="modalForm1">
+							<Form.Control
+								type="text"
+								name="newCountryName"
+								onChange={(e) => setNewCountryName(e.target.value)}
+								value={newCountryName}
+								placeholder="Enter a name to add a new country."
+								autoFocus
+								autoComplete="off"
+							/>
+						</Form.Group>
+					</Toast.Body>
+				</Toast>
+			</ToastContainer>
+		</React.Fragment>
+	);
+};
 
 export default NewCountry;
